33081732467264124706c2364e8d873aa8f93b0d
Muhammed Midlaj <muhammedmidlaj8395@gmail.com>
2024-06-26 16:31:33 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:weather_app/home/home_page.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/controller/provider/weather_data.dart';
+import 'package:weather_app/view/home/home_page.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(
@@ -14,12 +16,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          scaffoldBackgroundColor: const Color.fromARGB(255, 2, 21, 36)),
-      home: HomePaage(),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => WeatherData())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            scaffoldBackgroundColor: const Color.fromARGB(255, 2, 21, 36)),
+        home: HomePage(),
+      ),
     );
   }
 }
